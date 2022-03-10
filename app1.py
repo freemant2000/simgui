de65e82f28ecc38d1aa9e63818629af22c5b1552
Kent Tong <kent.tong.mo@gmail.com>
2022-03-10 19:28:33 +0800

    add test app1

---
+++ app1.py
@@ -0,0 +1,17 @@
+from simgui import *
+
+def on_ready():
+    add_label("l1", "Hello")
+    add_label("l2", "Hi")
+    add_input("i1")
+    add_input("i2")
+    add_button("b1", "OK")
+
+def on_click_b1():
+    set_label_text("l2", 333)
+
+def on_edited_i1():
+    set_input_text("i2", get_input_text("i1"))
+
+start(globals())
+
